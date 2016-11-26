a1a4bdb3927cbbc949e01fa712ca1964b8b29936
rgylling <mikegylling@yahoo.com>
2016-11-25 18:32:50 -0800

    adding basics to app controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
 
   register Sinatra::Twitter::Bootstrap::Assets
+
+  configure do
+  set :public_folder, 'public'
+  set :views, 'app/views'
+  enable :sessions
+  set :session_secret, "mysecret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
 end
